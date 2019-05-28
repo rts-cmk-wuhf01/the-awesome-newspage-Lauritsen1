91b639bc3af206d084360ef13a61251ef5d5a4d9
Rasmus Lauritsen <339185@rts-365.dk>
2019-05-28 14:02:00 +0200

    lavet alle routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,6 +4,19 @@ module.exports = (app) => {
       res.render('home');
    });
 
+   app.get('/catagories', (req, res, next) => {
+      res.render('catagories-post');
+   });
+
+   app.get('/single-post', (req, res, next) => {
+      res.render('single-post');
+   });
+
+
+   app.get('/about', (req, res, next) => {
+      res.render('about');
+   });
+
    app.get('/contact', (req, res, next) => {
       res.render('contact');
    });
