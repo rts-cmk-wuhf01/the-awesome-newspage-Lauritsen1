657058a1563e55a880c14f2001ba108cd6b712a5
Rasmus Lauritsen <339185@RTS-365.dk>
2019-06-14 12:45:34 +0200

    hentet site info fra database

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,50 +5,60 @@ module.exports = (app) => {
    app.get('/', async (req, res, next) => {
       let db = await mysql.connect();
       let [categories] = await db.execute('SELECT * FROM categories');
+      let [siteInfo] = await db.execute('SELECT * FROM site_info');
       db.end();
 
       res.render('home', {
-         "categories": categories
+         "categories": categories,
+         "siteInfo": siteInfo
       });
    });
 
    app.get('/catagories', async (req, res, next) => {
       let db = await mysql.connect();
       let [categories] = await db.execute('SELECT * FROM categories');
+      let [siteInfo] = await db.execute('SELECT * FROM site_info');
       db.end();
 
       res.render('categories-post', {
-         "categories": categories
+         "categories": categories,
+         "siteInfo": siteInfo
       });
    });
 
    app.get('/single-post', async (req, res, next) => {
       let db = await mysql.connect();
       let [categories] = await db.execute('SELECT * FROM categories');
+      let [siteInfo] = await db.execute('SELECT * FROM site_info');
       db.end();
 
       res.render('single-post', {
-         "categories": categories
+         "categories": categories,
+         "siteInfo": siteInfo
       });
    });
 
    app.get('/about', async (req, res, next) => {
       let db = await mysql.connect();
       let [categories] = await db.execute('SELECT * FROM categories');
+      let [siteInfo] = await db.execute('SELECT * FROM site_info');
       db.end();
 
       res.render('about', {
-         "categories": categories
+         "categories": categories,
+         "siteInfo": siteInfo
       });
    });
 
    app.get('/contact', async (req, res, next) => {
       let db = await mysql.connect();
       let [categories] = await db.execute('SELECT * FROM categories');
+      let [siteInfo] = await db.execute('SELECT * FROM site_info');  
       db.end();
 
       res.render('contact', {
-         "categories": categories
+         "categories": categories,
+         "siteInfo": siteInfo
       });
    });
 
