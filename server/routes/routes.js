3d236288495acf85df959899ac36d5d61540b5f9
Rasmus Lauritsen <339185@rts-365.dk>
2019-06-19 12:51:03 +0200

    lavet route som sortere recent post efter dato/tid

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,22 +6,24 @@ module.exports = (app) => {
       let db = await mysql.connect();
       let [categories] = await db.execute('SELECT * FROM categories');
       let [siteInfo] = await db.execute('SELECT * FROM site_info');
+      let [recentArticles] = await db.execute('SELECT * FROM articles ORDER BY article_postdate');
       db.end();
 
       res.render('home', {
          "categories": categories,
-         "siteInfo": siteInfo
+         "siteInfo": siteInfo,
+         "recentArticles": recentArticles
       });
    });
 
-   app.get('/catagories', async (req, res, next) => {
+   app.get('/catagories/:category_id', async (req, res, next) => {
       let db = await mysql.connect();
-      let [categories] = await db.execute('SELECT * FROM categories');
+      let [categories] = await db.execute('SELECT * FROM categories WHERE category_id = ?', [1]);
       let [siteInfo] = await db.execute('SELECT * FROM site_info');
       db.end();
 
       res.render('categories-post', {
-         "categories": categories,
+         "categories": categories[0],
          "siteInfo": siteInfo
       });
    });
